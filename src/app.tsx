fc487f72c07c5121a2b1bed72c80056dadb778e7
Tyler Liu <tyler4long@gmail.com>
2024-08-24 10:41:09 -0700

    Copy some code from manate

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,7 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect, useRef, useState } from 'react';
 import { Button } from 'antd';
+import { manage, run, disposeSymbol } from 'manate';
+import type { ManateEvent } from 'manate/models';
 
 const App = () => {
   const [count, setCount] = React.useState(0);
@@ -13,12 +15,41 @@ const App = () => {
   );
 };
 
-const _Child = () => {
-  console.log('Child renders');
-  return <h2>Child</h2>;
+const auto = (Component: React.FC) => {
+  const temp = (props) => {
+    const render = () => Component(props);
+    const prev = useRef<() => void>();
+    prev.current?.();
+    const dispose = () => {
+      managed?.[disposeSymbol]();
+      managed = undefined;
+    };
+    prev.current = dispose;
+    useEffect(() => {
+      if (!managed) {
+        // strict mode re-mount
+        managed = manage(props);
+        managed.$e.on(listener);
+      }
+      return dispose;
+    }, []);
+    let managed = manage(props);
+    const [result, isTrigger] = run(managed, render);
+    const [, refresh] = useState(0);
+    const listener = (event: ManateEvent) => {
+      if (isTrigger(event)) {
+        refresh((i) => i + 1);
+      }
+    };
+    managed.$e.on(listener);
+    return result;
+  };
+  return memo(temp);
 };
 
-const Child = memo(() => null);
-Child.type = _Child;
+const Child = auto(() => {
+  console.log('Child renders');
+  return <h2>Child</h2>;
+});
 
 export default App;
